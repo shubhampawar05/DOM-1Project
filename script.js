ec05136bb1047d4b19c7c74c34ab7de824e3ef2b
Shubham Chopde <shubhamchopde090@gmail.com>
2024-01-07 12:21:38 +0530

    adding file

---
+++ script.js
@@ -0,0 +1,12 @@
+let div = document.getElementById("box");
+
+div.style.backgroundColor = "yellow";
+div.style.margin= "20px";
+div.style.padding = "10px";
+div.style.fontSize= "18px";
+div.style.fontWeight= "bold";
+div.style.height="200px";
+div.style.width="300px";
+
+
+
